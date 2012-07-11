7642461f03a41e33ff94348a6266f7a12f4f8e7e
barberj <barber.justin@gmail.com>
2012-07-11 19:01:27 -0400

    adding setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='bridge-examples',
+    version='0.0.1',
+    author='Justin',
+    author_email='barber.justin(at)gmail.com',
+    install_requires=[
+        "tornado >= 2.2",
+    ]
+)
