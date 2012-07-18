d8ba2bf4ceb2bcd747a3857888082d5a836443a5
barberj <barber.justin@gmail.com>
2012-07-17 20:54:20 -0400

    serialize datetime to send

--- datetime_test/server.py
+++ datetime_test/server.py
@@ -4,10 +4,14 @@ config = conf_from_file('../development.py')
 
 from BridgePython import Bridge
 
+class Retval(object):
+    def __json__(self):
+        return dict(timestamp=datetime.now().isoformat())
+
 class SimpleHandler(object):
     def simple(self, callback=None):
         if callback:
-            callback(datetime.now())
+            callback(Retval().__json__())
 
 bridge = Bridge(api_key=config.private_api_key)
 bridge.publish_service('simple', SimpleHandler())
